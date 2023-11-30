6e117f188824440fde381da114820c6792e73d2c
Hardik Kapadia <thecoderenroute@gmail.com>
2023-11-29 21:26:18 -0600

    fixed minor bugs

--- fetch_stock_data_for_date.py
+++ fetch_stock_data_for_date.py
@@ -10,6 +10,7 @@ class StockData:
         stock_data = self.df[(self.df['Date'] == str(exact.date())) & (self.df['Ticker'] == ticker)]
         
         if stock_data.shape[0] > 0:
+            stock_data = stock_data.iloc[0]
             return stock_data.to_dict()
         elif prefer_before:
             td = timedelta(days=-1)
@@ -22,31 +23,30 @@ class StockData:
             exact += td
             
             stock_data = self.df[(self.df['Date'] == str(exact.date())) & (self.df['Ticker'] == ticker)]
-            stock_data = stock_data.iloc[0]
+            
             if stock_data.shape[0] > 0:
+                stock_data = stock_data.iloc[0]
                 return stock_data.to_dict()
             
-        raise ValueError("Invalid date or ticker")
+        raise ValueError(f"Invalid date: {str(exact.date())} or ticker {ticker}")
         
-    def get_for(self,timestamp: int, ticker: str) -> tuple:
+    def get_delta(self,timestamp: int, ticker: str) -> tuple:
         
         exact = datetime.fromtimestamp(timestamp)
         stock_data_1 = self.fetch_stock_date_for_date(exact, ticker)
         
-        print(f'Sd1: {stock_data_1}')
         close = stock_data_1['Close']
         
         if exact.hour < 14:
             open = stock_data_1['Open']
             return close - open
         else:
-            stock_data_2 = self.fetch_stock_date_for_date((exact + timedelta(days=24)).date(),ticker, False)
-            print(f'Sd2: {stock_data_2}')
+            stock_data_2 = self.fetch_stock_date_for_date(exact + timedelta(days=1),ticker, False)
             open = stock_data_2['Open']
             return open - close
         
         
 if __name__ == "__main__":
     fd = StockData()
-    difference = fd.get_for(1697896800, "WMT")
+    difference = fd.get_delta(1699742439, "AAPL")
     print(difference)
